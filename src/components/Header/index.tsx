24fde2a2b7c060d6c45e1623872f21ca7770faa9
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-09-15 19:58:23 +0600

    fix: fonts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import "./header.scss"
 import HeaderUp from "./HeaderUp";
-import HeaderDown from "./HeaderDown/HeaderDown";
 
 
 const Index = () => {
     return (
-        <div className="header">
+        <header className="header">
             <HeaderUp/>
-        </div>
+        </header>
     );
 };
 
