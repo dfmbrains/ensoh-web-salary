4cd4efac8f6ca88bc178b7818ff3cf67b94a03fc
Nurekov <killerbuny3@gmail.com>
2022-09-15 20:08:15 +0600

    last commit

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -8,18 +8,29 @@ const CalendApp = () => {
     const [date, setDate] = useState(new Date());
 
     const weekdays = ['ש','ו','ה','ד','ג','ב','א'];
+    const workDays = [3, 1, 8, 7, 6, 5, 4, 15, 14, 13, 12, 19, 18, 25];
+    const errorDays = [11, 21, 20];
 
     useEffect(()=>{
         document.querySelector('.react-calendar__month-view__weekdays')
-            .querySelectorAll('abbr').forEach((item, idx)=>{
+            .querySelectorAll('abbr').forEach((item , idx)=>{
                 item.textContent = weekdays.reverse()[idx]
         })
+        document.querySelectorAll('.react-calendar__tile')
+            .forEach((item, idx)=>{
+                if (workDays.includes(+item.textContent) && !item.className.includes('active')){
+                    item.classList.add('workDay')
+                }
+                else if (errorDays.includes(+item.textContent) && !item.className.includes('active')){
+                    item.classList.add('errorDays')
+                }
+            })
     }, []);
 
     return (
         <div className='Calendar'>
             <div className='calendar-container'>
-                <Calendar locale="iw-IW" calendarType='Hebrew' onChange={setDate} value={date}/>
+                <Calendar defaultValue={new Date()} locale="iw-IW" calendarType='Hebrew' onChange={setDate} value={date}/>
             </div>
             <div className="tips">
                 <div className="tip">
