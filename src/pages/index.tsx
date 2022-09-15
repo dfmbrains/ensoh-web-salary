09b6e7c2fa251932a7280724592f812f7818c84a
Nurekov <killerbuny3@gmail.com>
2022-09-15 12:03:11 +0600

    header up some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from "./Home";
+import Header from "../components/Header";
 
 const Routing = () => {
     return (
         <>
+            <Header/>
             <main>
                 <Routes>
                     <Route path={"/"} element={<Home/>}/>
